b74df32ee456d4dfcf6674b5734d4ff9728caca0
YxTiBlya <87864991+YxTiBlya@users.noreply.github.com>
2022-01-29 23:46:45 +0400

    Create pch.h

---
+++ pch.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef PCH_H
+#define PCH_H
+
+#include "framework.h"
+#include "stdio.h"
+#include <tchar.h>
+
+#include <iostream>
+#include <vector>
+#include <TlHelp32.h>
+
+#endif
